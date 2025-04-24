b3732a72486d9488c220bbd0fe3e20706c75d8e9
Zoimback <rgalexmv@gmail.com>
2025-04-24 10:57:50 +0200

    Prueba builder.groovy

--- vars/Builder.groovy
+++ vars/Builder.groovy
@@ -10,8 +10,8 @@ void call(){
            properties([
                 parameters([
                     //choice(name: 'ENTORNO', choices: ['desarrollo', 'producción'], description: 'Selecciona el entorno de despliegue')
-                    choice(name: 'Rehacer Imagen', choices: ['Si', 'No'], description: 'Selecciona si quieres rehacer la imagen de docker'),
-                    choice(name: 'Imagen', choices: ['api-sensor', 'sensor'], description: 'Selecciona que imagen de docker quieres construir')
+                    //choice(name: 'Rehacer Imagen', choices: ['Si', 'No'], description: 'Selecciona si quieres rehacer la imagen de docker'),
+                    //choice(name: 'Imagen', choices: ['api-sensor', 'sensor'], description: 'Selecciona que imagen de docker quieres construir')
                     ])
                 ])
         }
@@ -20,14 +20,14 @@ void call(){
 
         stage('Checkout') {
             def gitUtils = new GitUtils(this) //Contexto de la pipeline
-            gitUtils.cloneRepository('develop', "https://github.com/Zoimback/${params['Imagen']}.git")
+            def url = scm?.getUserRemoteConfigs()?.first()?.getUrl() ?: 'Desconocido'
+            def rama = env.BRANCH_NAME
+            gitUtils.cloneRepository(rama, url)
         }
 
-        if (params['Rehacer Imagen'] == 'Si' ) {
-            stage('Delete-Docker Image') {
-                dockerUtils.removeImage("params['Imagen']")
-                dockerUtils.buildImage("params['Imagen']", "${env.WORKSPACE}/Dockerfile", "${env.WORKSPACE}")
-            }
+        stage('Delete-Docker Image') {
+            
+            dockerUtils.buildImage("params['Imagen']", "${env.WORKSPACE}/Dockerfile", "${env.WORKSPACE}") 
         }
         stage('Build-Docker Image') {
             dockerUtils.buildImage("params['Imagen']", "${env.WORKSPACE}/Dockerfile", "${env.WORKSPACE}")
@@ -41,15 +41,15 @@ void call(){
             dockerUtils.removeContainer("params['Imagen']")
         }
 
-        /**stage('Delete-Docker Image') {
+        stage('Delete-Docker Image') {
             def dockerUtils = new DockerUtils(this) //Contexto de la pipeline
             dockerUtils.removeImage('api-sensor')
-        }*/
-
+        }
+        /*
         stage('DB Connection') {
             def dbconector = new DBconector(this) //Contexto de la pipeline
             dbconector.executeQuery("SELECT * FROM audit")
-        }
+        }*/
 
         stage('Clean') {
             cleanWs() // Limpia el workspace
